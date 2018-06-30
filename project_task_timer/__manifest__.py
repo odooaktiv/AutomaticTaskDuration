193f0353a95eba5fc8f37c85cc27a319618af771
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 16:38:40 +0530

    [ADD] initial commit ProjectTaskTimer for branch 10.0.

---
+++ project_task_timer/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full
+# copyright and licensing details.
+
+{
+    'name': 'Project Task Timer',
+    'version': '10.0.1.0.0',
+    'category': 'Project',
+    'summary': '''
+        Calculates project task duration automatically.
+        ''',
+    'website': 'http://www.aktivsoftware.com',
+    'author': 'Aktiv Software',
+    # 'license': "OPL-1",
+    'depends': [
+        'hr_timesheet'
+    ],
+    'website': 'www.aktivsoftware.com',
+    'data': [
+        'views/project_timesheet_view.xml',
+        'wizards/task_entry_view.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+}
